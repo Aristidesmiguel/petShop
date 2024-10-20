2b684f1c433e78ef5061f1012e52a236f9aa1eee
Aristidesmiguel <miguelaristides21@gmailcom>
2024-10-20 23:13:12 +0100

    feito ola mundo

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+//DOOM - UMA TECNICÁ QUE USADA PELOS PROGRAMDORES. TEM COMO FUNÇÃO, MANIPULAR ELEMENTOS HTML ATRAVÉS DO JAVASCRIPT OU UMA LINGUAGEM DE PROGRAMAÇÃO
+const button = document.getElementById('btn').addEventListener('click', () => handleOnClickButton())
+const botao = document.getElementById('botao').addEventListener('click', () => handleOnClickButton())
+
+//getElementById - BUSCANDO O ELEMENTO PELO ID
+
+//quanto chicado no botão executa esta função: handleOnClickButton
+const handleOnClickButton = () => {
+        alert('Esta função ainda não foi adicionada!')
+}
